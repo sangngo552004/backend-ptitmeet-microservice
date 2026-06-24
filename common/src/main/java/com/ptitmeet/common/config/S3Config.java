f2ae8a1cb31717faab5efcc09294b7148a226bd0
Ngo Tan Sang <sangngo552004@gmail.com>
2026-06-24 15:31:14 +0700

    fix: fix bug for each service

--- common/src/main/java/com/ptitmeet/common/config/S3Config.java
+++ common/src/main/java/com/ptitmeet/common/config/S3Config.java
@@ -1,6 +1,8 @@
 package com.ptitmeet.common.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -9,6 +11,8 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
+@ConditionalOnProperty(prefix = "aws.s3", name = "enabled", havingValue = "true")
+@EnableConfigurationProperties(S3Properties.class)
 @RequiredArgsConstructor
 public class S3Config {
 
--- common/src/main/java/com/ptitmeet/common/config/S3Properties.java
+++ common/src/main/java/com/ptitmeet/common/config/S3Properties.java
@@ -2,9 +2,7 @@ package com.ptitmeet.common.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "aws.s3")
 @Data
 public class S3Properties {
